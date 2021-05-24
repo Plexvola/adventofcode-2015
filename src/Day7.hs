901585604196065554c051f8c0194c104784b6d7
Plexvola <thinkplex@riseup.net>
2021-05-24 22:08:38 +0200

    day7 finished

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,2 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Day7 where
 
+import Data.List.Split
+import Data.Bits
+import Data.Char
+import Data.Word
+
+example :: [String]
+example = ["123 -> x", "456 -> y", "x AND y -> d", "x OR y -> e", "x LSHIFT 2 -> f", "y RSHIFT 2 -> g", "NOT x -> h", "NOT y -> i"]
+
+tupleWire :: String -> (String, String)
+tupleWire w = (head $ splitOn " -> " w, last $ splitOn " -> " w)
+
+wires :: [String] -> [(String, String)]
+wires = map tupleWire
+
+checkGate :: String -> String -> Bool
+checkGate g = (/=1) . length . splitOn g
+
+wireSignal :: [(String, String)] -> String -> Word16
+wireSignal ws o
+    | checkGate "AND" o    =
+      wireSignal ws (head $ splitOn " AND " o) .&. wireSignal ws (last $ splitOn " AND " o)
+    | checkGate "OR" o     =
+      wireSignal ws (head $ splitOn " OR " o) .|. wireSignal ws (last $ splitOn " OR " o)
+    | checkGate "NOT" o    = complement $ wireSignal ws (last $ splitOn "NOT " o)
+    | checkGate "LSHIFT" o =
+      shiftL (wireSignal ws (head $ splitOn " LSHIFT " o)) (fromIntegral $ wireSignal ws (last $ splitOn " LSHIFT " o))
+    | checkGate "RSHIFT" o =
+      shiftR (wireSignal ws (head $ splitOn " RSHIFT " o)) (fromIntegral $ wireSignal ws (last $ splitOn " RSHIFT " o))
+    | all isDigit o        = read o
+    | otherwise            = wireSignal ws . fst . head . filter ((==o).snd) $ ws
+
+runDay7 = do
+  fn <- readFile "data/day7_input.txt"
+  let wireArray = wires $ lines fn
+  let signalWireA = wireSignal wireArray "a"
+  print signalWireA
--- src/Main.hs
+++ src/Main.hs
@@ -7,17 +7,17 @@ import Day6
 import Day7
 
 main = do
-    putStrLn "===DAY 1==="
-    runDay1
-    putStrLn "===DAY 2==="
-    runDay2
-    putStrLn "===DAY 3==="
-    runDay3
-    putStrLn "===DAY 4==="
-    runDay4
-    putStrLn "===DAY 5==="
-    runDay5
-    putStrLn "===DAY 6==="
-    runDay6
+    -- putStrLn "===DAY 1==="
+    -- runDay1
+    -- putStrLn "===DAY 2==="
+    -- runDay2
+    -- putStrLn "===DAY 3==="
+    -- runDay3
+    -- putStrLn "===DAY 4==="
+    -- runDay4
+    -- putStrLn "===DAY 5==="
+    -- runDay5
+    -- putStrLn "===DAY 6==="
+    -- runDay6
     putStrLn "===DAY 7==="
     runDay7
